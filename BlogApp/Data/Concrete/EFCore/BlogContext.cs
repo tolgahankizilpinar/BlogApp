4ba8ac7acaa5d87857b9f7e5514c0f68f29f1fd7
Your Name <tolgahan.kizilpinar@hotmail.com>
2025-06-15 23:34:27 +0300

    DbContext-ConnString-Migrations

---
+++ BlogApp/Data/Concrete/EFCore/BlogContext.cs
@@ -0,0 +1,18 @@
+using BlogApp.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.Data.Concrete.EFCore
+{
+    public class BlogContext :DbContext
+    {
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
