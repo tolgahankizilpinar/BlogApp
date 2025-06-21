25db92f35817a438394987dbe78b1423b135bc0e
Your Name <tolgahan.kizilpinar@hotmail.com>
2025-06-21 21:24:14 +0300

    Post Details

--- BlogApp/Controllers/PostsController.cs
+++ BlogApp/Controllers/PostsController.cs
@@ -1,6 +1,7 @@
 using BlogApp.Data.Abstract;
 using BlogApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogApp.Controllers
 {
@@ -21,6 +22,11 @@ namespace BlogApp.Controllers
                     Posts = _postRepository.Posts.ToList()
                 }
             );
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            return View(await _postRepository.Posts.FirstOrDefaultAsync(p => p.PostId == id));
         }
     }
 }
